cf36d32ab29e0350640d9edfd9ec36e27a5d609f
erzhiqianyi <github@caofeng.me>
2023-08-31 22:34:54 +0800

    select words from context menu and show in the alert

--- extention/scripts/content-script.js
+++ extention/scripts/content-script.js
@@ -1,1 +1,17 @@
 console.log("start content service")
+
+//add message listener
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+    const action = message.action
+    switch (action) {
+        case "AddToAnki":
+            const selectedText = message.selectedText;
+            showSelectedText(selectedText)
+            break
+    }
+});
+
+function showSelectedText(selectedText){
+    const selectedWords = selectedText.trim().split(/\s+/);
+    alert("selected text is: "+selectedWords.join("\n"));
+}
